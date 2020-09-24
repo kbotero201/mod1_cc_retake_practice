5662111edd82e0ce08895fdfe3adc748f75d4208
kbotero201 <kbotero201@gmail.com>
2020-09-23 21:58:07 -0400

    class MenuItem

---
+++ app/models/menu_item.rb
@@ -0,0 +1,19 @@
+
+class MenuItem
+
+    attr_accessor :restaurant, :recipe, :price
+    
+    @@all = []
+
+    def initialize(restaurant, recipe, price)
+        @restaurant = restaurant
+        @recipe = recipe
+        @price = price
+
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+end 
