1470839fe4273920075d10d8817bb356ab73298f
kbotero201 <kbotero201@gmail.com>
2020-09-23 21:48:29 -0400

    recipe class

---
+++ app/models/recipe.rb
@@ -0,0 +1,19 @@
+
+class Recipe 
+
+    attr_reader :name
+    attr_accessor :description
+    @@all = []
+
+    def initialize(name, description)
+        @name = name 
+        @description = description
+
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+end 
